cb013630a720e3ff01cc1c2c95cc8fc420d4eaf8
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-04 18:30:49 -0400

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,22 @@
 const request = require('request');
-const name = process.argv[2];
 
-
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
-  // console.log(typeof body)
-  if (error) {
-    console.log('error:', error);
-    return;
-  }
-
-  const data = JSON.parse(body);
- 
-  if (data.length === 0) {
-    console.log(`Breed not found`);
-    return;
-  }
- 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      callback(`Breed not found`);
+      return;
+    }
+    callback(null, data[0].description);
   
-  console.log(data[0].description);
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  
-});
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
